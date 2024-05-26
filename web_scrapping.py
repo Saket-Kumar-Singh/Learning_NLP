8f6098a21f79591ed79b53924a0e5bccb44dd112
Saket-Kumar-Singh <saketkrsingh37772@gmail.com>
2024-05-27 01:14:46 +0530

    Commit(#2)

--- web_scrapping.py
+++ web_scrapping.py
@@ -8,6 +8,11 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import pandas as pd
 
+def progress_bar(progress, total):
+    percentage = progress/ float(total) * 100
+    bar = "█"*int(percentage) +  "-"*(100 - int(percentage))
+    print(f"\r| {bar} | {percentage: 0.2f}%", end = "\r")
+
 chrome_options = Options()
 chrome_options.add_experimental_option("detach",True)
 
@@ -28,7 +33,9 @@ urls = []
 box_ele = driver.find_element("xpath" , "//*[@id=\"search-results\"]/div[2]")
 # print(box_ele)//*[@id="search-results"]/div[2]/div[20]/div[1]/div/div/div[1]/div/div/div[1]/h2/a
 button = driver.find_element("xpath", f"//*[@id=\"search-results\"]/div[3]/div/div/div/a[3]")
-for j in range(32):
+print("Collecting URLS")
+progress_bar(0, 625)
+for j in range(31):
     for i in range(20): 
         try:
             ele = driver.find_element("xpath", f"//*[@id=\"search-results\"]/div[2]/div[{i+1}]/div[1]/div/div/div[1]/div/div/div[1]/h2/a")
@@ -38,15 +45,22 @@ for j in range(32):
     try:    
         button = driver.find_element("xpath", f"//*[@id=\"search-results\"]/div[3]/div/div/div/a[3]")
         button.click()
+        progress_bar((i+1) + 20*(j) , 20*32)
     except:
-        print(f"Button clicking issue at page {j+1}")
-        break    
+        print("|", end = "\n")
+        print(f"\nButton clicking issue at page {j+1}", end = "\n")
+        break  
 
+progress_bar(625, 625)
+print("|", end = "\n")
+print("Collecting data from urls")
+progress_bar(0, len(urls))
 data = []
 
-for Url in urls:
+for prog, Url in enumerate(urls):
     txt = {} 
-    print(f"Extracting data from URL : {Url}")
+    # print(f"Extracting data from URL : {Url}")
+    progress_bar(prog + 1, len(urls))
     html_text = requests.get(Url).text
     soup = BeautifulSoup(html_text, 'lxml')
 
@@ -68,14 +82,14 @@ for Url in urls:
         txt[column.find('h5').text.replace(' ', '').replace('\n','').replace('\t','')] = column.find('li').text.replace(' ', '').replace('\n','').replace('\t','')
     
     durations = project_duration.find_all('div', class_ = 'box-content-column')
-    # print(durations)
     for duration in durations:
-        # print()
         txt[duration.find('strong').text.replace(' ', '').replace('\n','').replace('\t','')[:-1]] = duration.text.replace(' ', '').replace('\n','').replace('\t','').split(':')[1]
 
     data.append(txt)
 
 # print(data)
+print("\n", end = "\n")
+print("Completed! Making csv")
 data = pd.DataFrame(data)
 data.to_csv("Quantum_Mission_Data.csv")
 # print(data)
